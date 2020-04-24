ac1fe731fed59e73fa6ff8fcc644515206b04322
Omar El Hefny <o.hefny@gmail.com>
2020-04-24 22:21:59 +0200

    feat: add api interface to handle dealing with data

---
+++ src/api.js
@@ -0,0 +1,15 @@
+import { _getQuestions, _getUsers, _saveQuestion, _saveQuestionAnswer } from "./_DATA";
+
+
+export function getUsers(){
+    return _getUsers()
+}
+export function getQuestions(){
+    return _getQuestions
+}
+export function createQuestion(questionData){  //{ optionOneText, optionTwoText, author }
+    return _saveQuestion(questionData)
+}
+export function answerQuestion(answerData){ //{ authedUser, qid, answer }
+    return _saveQuestionAnswer(answerData)
+}
