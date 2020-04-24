354c8b6d7d3eaf11005ac5836b60fab4f69084e3
Omar El Hefny <o.hefny@gmail.com>
2020-04-24 22:22:44 +0200

    feat: handle recieve users action

--- src/actions/users.js
+++ src/actions/users.js
@@ -1,8 +1,16 @@
-const RECEIVE_USERS = "RECEIVE_USERS"
+import { getUsers } from "../api";
 
-export function receiveUsers(users){
-    return {
-        type:RECEIVE_USERS,
-        users
-    }
+const RECEIVE_USERS = "RECEIVE_USERS";
+
+function receiveUsers(users) {
+  return {
+    type: RECEIVE_USERS,
+    users,
+  };
+}
+
+export default function handleReceiveUsers() {
+  return (dispatch) => {
+    getUsers.then((users) => dispatch(receiveUsers(users)));
+  };
 }
