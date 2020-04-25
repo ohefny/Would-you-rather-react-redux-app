bd23af6e7fc3dd304b9826234774de599e8db6a1
Omar El Hefny <o.hefny@gmail.com>
2020-04-25 13:08:53 +0200

    feat: add ui mappers for poll stats and leaders board

---
+++ src/components/ui_mappers.js
@@ -0,0 +1,40 @@
+export function getPollStatsUIModel(poll, authedUser) {
+  const totalVotes =
+    (poll.optionOne.votes.length + poll.optionTwo.votes.length) * 1.0;
+  const optionOnePercentage = (
+    (poll.optionOne.votes.length / totalVotes) *
+    100
+  ).toPrecision(2);
+  const optionTwoPercentage = 100 - optionOnePercentage;
+
+  return {
+    optionOne: {
+      stats: optionOnePercentage,
+      statsText: `${poll.optionOne.votes.length} out of ${totalVotes}`,
+      text: poll.optionTwo.text,
+      isChosen: poll.optionOne.votes.includes(authedUser),
+    },
+    optionTwo: {
+      stats: optionTwoPercentage,
+      statsText: `${poll.optionTwo.votes.length} out of ${totalVotes}`,
+      text: poll.optionTwo.text,
+      isChosen: poll.optionTwo.votes.includes(authedUser),
+    },
+  };
+}
+export function getLeaderBoardUIModel(users) {
+  const sortedUsers = [...users].sort((u1, u2) => getScore(u2) - getScore(u1));
+  return sortedUsers.map(mapUserToLeader);
+}
+
+function mapUserToLeader({ name, avatarURL, answers, questions }) {
+  return {
+    name,
+    avatarURL,
+    score: answers.length+questions.length,
+    answersCount: answers.length,
+    questionsCount: questions.length,
+  };
+}
+
+const getScore = (user) => user.questions.length + user.answers.length;
