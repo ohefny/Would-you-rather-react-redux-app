ce17f6e7878dd272f4abecf9555fa518f09bf2cf
Omar El Hefny <o.hefny@gmail.com>
2020-05-09 01:28:38 +0200

    feat: add ui of PollContent

--- src/components/PollContent.js
+++ src/components/PollContent.js
@@ -1,11 +1,53 @@
-import React,{ Component } from "react";
+import React, { Component } from "react";
 import { connect } from "react-redux";
-
+import { Form, Button } from "react-bootstrap/";
 class PollContent extends Component {
-
-    render(){
-        return <div></div>
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      checkedOption: 1,
+    };
+  }
+  onSubmitClicked = (e) => {
+    //todo call dispatcher with proper action
+  };
+  onChoiceChanged = (e, option) => {
+    //e.preventDefault()
+    this.setState({ checkedOption: option });
+  };
+  render() {
+    return (
+      <div>
+        <Form key="optionOne" className="mb-3">
+          <Form.Label style={{ fontSize: "22px", fontWeight: "700" }}>
+            Would You Rather ...
+          </Form.Label>
+          <Form.Check
+            checked={this.state.checkedOption === 1}
+            onChange={(e) => this.onChoiceChanged(e, 1)}
+            type="radio"
+            id="optionOne"
+            label={this.props.optionOne.text}
+          />
+          <Form.Check
+            checked={this.state.checkedOption === 2}
+            onChange={(e) => this.onChoiceChanged(e, 2)}
+            type="radio"
+            label={this.props.optionOne.text}
+            id="optionTwo"
+          />
+          <Button
+            onClick={this.onSubmitClicked}
+            variant="primary"
+            type="submit"
+            style={{ height: "40px" }}
+          >
+            Submit
+          </Button>
+        </Form>
+      </div>
+    );
+  }
 }
 
 function mapStateToProps({ authedUser, polls }, { id }) {
